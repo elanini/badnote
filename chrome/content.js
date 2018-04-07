5654d7d73ff67e31cde4232d7a3f1c52b7224927
Eric Lanini <elanini@ucsc.edu>
2018-04-07 12:31:05 -0700

    add regex for post

--- chrome/content.js
+++ chrome/content.js
@@ -1,3 +1,8 @@
+function getPostId() {
+    let re = /class\/([^?]+)\?cid=(\d+)/;
+    let res = re.exec(window.location);
+    return (res[1] + res[2])
+}
 
 window.addEventListener('load', function() {
 console.log(document.cookie)
