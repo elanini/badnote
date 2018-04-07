0ce121abefb186ed620b2528eb8f994fe019e174
Eric Lanini <elanini@ucsc.edu>
2018-04-06 17:21:04 -0700

    initial server commit

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express()
+
+
+let posts = {}
+
+app.get('/:postid/:user', function (req, res) {
+    let pid = req.params.postid;
+    if (pid in posts) {
+        res.status(200).send('' + posts[pid].size)
+    } else {
+        res.status(200).send("1337")
+    }
+})
+
+
+app.post('/:postid/:user', function (req, res) {
+    let pid = req.params.postid;
+    let user = req.params.user;
+    if (pid in posts) {
+        posts[pid].add(user);
+    } else {
+        posts[pid] = new Set([user]);
+    }
+    res.sendStatus(200)
+})
+
+app.delete('/:postid/:user', (req, res) => {
+    let pid = req.params.postid;
+    let user = req.params.user;
+    if (pid in posts) {
+        posts[pid].delete(user);
+        res.sendStatus(200)
+    } else {
+        res.sendStatus(404)
+    }
+})
+
+
+
+app.listen(3001, () => console.log('Example app listening on port 3000!'))
