c55c617aadd583efbadf3cd09798124c1e0816c9
Eric Lanini <elanini@ucsc.edu>
2018-04-08 12:00:53 -0700

    update tests, await the db, no more db promise middlware bullshit

--- server/server.js
+++ server/server.js
@@ -32,17 +32,10 @@ async function get_user_status(db, pid, user) {
         }
 }
 
-exports.start = (name) => {
-    const dbPromise = Promise.resolve()
+exports.start = async (name) => {
+    const db = await Promise.resolve()
         .then(() => sqlite.open(name, { Promise }))
         .then(db => db.migrate({ force: 'last' }));
-
-    app.use((req, res, next) => {
-        dbPromise.then((db) => {
-            req.db = db;
-            next()
-        });
-    });
 
     app.use((req, res, next) => {
         console.log(`> ${req.method} ${req.url} ${req.body ? req.body : ""}`)
@@ -51,8 +44,6 @@ exports.start = (name) => {
 
     app.get('/:postid/:user', async function (req, res, next) {
         let {user, postid: pid} = req.params;
-        let db = req.db;
-        
 
         send(res, 200, await get_user_status(db, pid, user));
     })
@@ -60,20 +51,16 @@ exports.start = (name) => {
 
     app.post('/:postid/:user/', async function (req, res, next) {
         let {user, postid: pid} = req.params;
-        let db = req.db;
-
         
         let stmt = await db.prepare("INSERT OR IGNORE INTO posts VALUES (?, ?);");
         let _ = await stmt.run([pid, user])
         stmt.finalize();
 
         send(res, 200, await get_user_status(db, pid, user));
-        // send(res, 200);
     })
 
     app.delete('/:postid/:user', async (req, res, next) => {
         let {user, postid: pid} = req.params;
-        let db = req.db;
 
         let stmt = await db.prepare("DELETE FROM posts WHERE postid = ? AND userid = ?;")
         let _ = await stmt.run([pid, user])
@@ -84,5 +71,6 @@ exports.start = (name) => {
 
 
     app.listen(port, () => console.log('Listening on port ' + port))
+    return Promise.resolve()
 }
 
--- server/test.js
+++ server/test.js
@@ -1,59 +1,65 @@
 const assert = require('assert');
 const request = require('request-promise');
 
-const server = require('./server');
-server.start(':memory:');
-
 const base = 'http://localhost:3000';
 
+const server = require('./server');
+server.start(':memory:')
+.then(() => {
+    test()
+})
+
+
 async function test() {
+    // should be empty, not voted
     let res = await request.get(base + '/a/b');
     assert.deepStrictEqual(JSON.parse(res),
         {
-            cnt: 0,
+            count: 0,
             voted: false
         });
 
+    // b vote on a
     res = await request.post(base + '/a/b');
-
-    res = await request.get(base + '/a/b');
-
     assert.deepStrictEqual(JSON.parse(res),
         {
-            cnt: 1,
+            count: 1,
             voted: true
         })
 
+    // check c has not voted
     res = await request.get(base + '/a/c');
-
     assert.deepStrictEqual(JSON.parse(res),
         {
-            cnt: 1,
+            count: 1,
             voted: false
         })
 
+    // c vote on a
     res = await request.post(base + '/a/c');
-    
-    res = await request.get(base + '/a/c');
-
     assert.deepStrictEqual(JSON.parse(res),
         {
-            cnt: 2,
+            count: 2,
             voted: true
         })
 
-    res = await request.delete(base + '/a/c');
-    
-    res = await request.get(base + '/a/c');
-
+    // c cant vote twice
+    res = await request.post(base + '/a/c');
     assert.deepStrictEqual(JSON.parse(res),
         {
-            cnt: 1,
+            count: 2,
+            voted: true
+        })
+
+    // delete c's vote
+    res = await request.delete(base + '/a/c');
+    assert.deepStrictEqual(JSON.parse(res),
+        {
+            count: 1,
             voted: false
         })
     console.log("pass")
     process.exit()
 }
 
-test()
 
